956a4e2ecd64c7685fa15cc1a4cc44d3c3968327
Restov <romankotovv@gmail.com>
2020-03-26 21:53:51 +0700

    Create lab 10.2

---
+++ lab10/lab10.1.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<fstream>
+#include<string.h>
+#include<stdio.h>
+#include<conio.h>
+#include <string>
+using namespace std;
+int main()
+{ 
+    string str;
+    ifstream fin; // буфер под файл который читаем
+    ofstream fout("output.txt");  // буфер под файл в который записываем
+    fin.open("input.txt"); // открываем файл
+    while (fin) // пока в файле нет 0 элементов
+    {
+        getline(fin, str); //считываем строку
+        for (int i = (str.length() - 1); i >= 0; i--) // вывод каждого символа по убыванию
+        {
+            fout << str[i];
+        }
+        fout << endl;
+    }
+    fin.close(); // закрытие файлов
+    fout.close();
+}
