608cb23feada0243ed161781a87bf1cdc1ceeb50
Restov <romankotovv@gmail.com>
2020-03-27 13:38:00 +0700

    Test Format

--- lab10/lab10.1.cpp
+++ lab10/lab10.1.cpp
@@ -9,72 +9,72 @@
 using namespace std;
 int main()
 {
-	ofstream fout;  // создаем объекты класса ofstream
-	ofstream sogl;
-	ofstream glasn;
-	ifstream fin; // создаем объекты класса ifstream
-	fout.open("text.txt"); // создание файлов
-	sogl.open("sogl.txt");
-	glasn.open("glasn.txt");
-	int n, i = 0, m = 0;
-	cout << "Enter the number of lines: ";
-	cin >> n;
-	string s[n];
-	cout << "\nEnter words: ";
-	cin.ignore(); // игнорирование '\n'
-	for(i = 0; i < n; ++i)
-	{	
-		getline(cin, s[i],'\n'); // считывание строк
-		fout << s[i] << "\n"; //запись строки в файл
-		m+= s[i].length(); //подсчет длин строк 
-	}
-	fout.close(); //закрываем файл
-	char st[m];
-	fin.open("text.txt"); //открываем файл для чтения 
-	i = 0;
-	while(fin.get(st[i])) //пока в файле нет 0 эл
-	{
-		if(st[i] == 'a' || st[i] == 'e' || st[i] == 'i' || st[i] == 'o' || st[i] == 'u' || st[i] == 'y')//проверка на наличие гласной  
-		{	
-			bool flag = true;
-			while(flag == true)
-			{
-   			    if(st[i] == ','||st[i] == ' '||st[i] == '\n') // проверка на наличие ' ', ',', '/n'
-   			    {
-   			    	flag = false;
-   			    	++i;
-   			    }
-   			    else
-   			    {
-				    glasn << st[i]; //записываем букву в файл
-				    ++i;
-					fin.get(st[i]); //считываем следующи символ с файла
-				}
-			}
-			glasn << " ";
-		}
-		else // если буква согласная 
-		{
-			bool flag = true;
-			while(flag==true)
-			{
-			    if(st[i] == ','||st[i] == ' '||st[i] == '\n' ) // проверка на наличие ' ', ',', '/n'
-			    {
-			    	flag = false;
-			    	++i;
-			    }
-			    else
-			    {
-				 	sogl << st[i]; //записываем букву в файл
-				 	++i;
-				  	fin.get(st[i]); //считываем следующий символ с файла
-				  }
-				}
-				sogl << " ";
-			}
-		}
-    	fin.close(); //закрываем файлы
-    	sogl.close();
-    	glasn.close();
-    	return 0;
+    ofstream fout;  // создаем объекты класса ofstream
+    ofstream sogl;
+    ofstream glasn;
+    ifstream fin; // создаем объекты класса ifstream
+    fout.open("text.txt"); // создание файлов
+    sogl.open("sogl.txt");
+    glasn.open("glasn.txt");
+    int n, i = 0, m = 0;
+    cout << "Enter the number of lines: ";
+    cin >> n;
+    string s[n];
+    cout << "\nEnter words: ";
+    cin.ignore(); // игнорирование '\n'
+    for(i = 0; i < n; ++i)
+    {   
+        getline(cin, s[i],'\n'); // считывание строк
+        fout << s[i] << "\n"; //запись строки в файл
+        m+= s[i].length(); //подсчет длин строк 
+    }
+    fout.close(); //закрываем файл
+    char st[m];
+    fin.open("text.txt"); //открываем файл для чтения 
+    i = 0;
+    while(fin.get(st[i])) //пока в файле нет 0 эл
+    {
+        if(st[i] == 'a' || st[i] == 'e' || st[i] == 'i' || st[i] == 'o' || st[i] == 'u' || st[i] == 'y')//проверка на наличие гласной  
+        {   
+            bool flag = true;
+            while(flag == true)
+            {
+                if(st[i] == ','||st[i] == ' '||st[i] == '\n') // проверка на наличие ' ', ',', '/n'
+                {
+                    flag = false;
+                    ++i;
+                }
+                else
+                {
+                    glasn << st[i]; //записываем букву в файл
+                    ++i;
+                    fin.get(st[i]); //считываем следующи символ с файла
+                }
+            }
+            glasn << " ";
+        }
+        else // если буква согласная 
+        {
+            bool flag = true;
+            while(flag==true)
+            {
+                if(st[i] == ','||st[i] == ' '||st[i] == '\n' ) // проверка на наличие ' ', ',', '/n'
+                {
+                    flag = false;
+                    ++i;
+                }
+                else
+                {
+                    sogl << st[i]; //записываем букву в файл
+                    ++i;
+                    fin.get(st[i]); //считываем следующий символ с файла
+                  }
+                }
+                sogl << " ";
+            }
+        }
+        fin.close(); //закрываем файлы
+        sogl.close();
+        glasn.close();
+        return 0;
     }
